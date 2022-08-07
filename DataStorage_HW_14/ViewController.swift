b28e2d8c81a29c5412770eb661a23f136ca158e9
Grachev Aleksandr <aleksandr.grachev@openmedia.co>
2022-08-07 03:59:27 +0300

    Initial Commit

---
+++ DataStorage_HW_14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataStorage_HW_14
+//
+//  Created by Aleksandr.Grachev on 07.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
